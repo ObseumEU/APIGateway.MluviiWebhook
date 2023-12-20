128f105a5d2c4c652e18c4db1ca3f769cadb751d
David Rosko <rosko.dav@gmail.com>
2023-12-20 21:29:56 +0100

    Add rabbitMQ masstransit and refactor program.cs

--- APIGateway.MluviiWebhook/Program.cs
+++ APIGateway.MluviiWebhook/Program.cs
@@ -1,101 +1,47 @@
-using APIGateway.Core;
-using APIGateway.Core.Cache;
-using APIGateway.Core.Kafka;
-using APIGateway.Core.MluviiClient;
 using APIGateway.MluviiWebhook;
-using APIGateway.MluviiWebhook.Jobs;
 using Microsoft.FeatureManagement;
 using Sentry;
-using Silverback.Samples.Kafka.Batch.Producer;
 
 var builder = WebApplication.CreateBuilder(args);
-var config = builder.Configuration;
-var services = builder.Services;
-
-// Add services to the container.
-builder.Services.AddFeatureManagement();
-services.AddLogging(builder =>
-    builder
-        .AddDebug()
-        .AddConsole()
-        .AddConfiguration(config.GetSection("Logging"))
-        .SetMinimumLevel(LogLevel.Information)
-);
-builder.Services.AddControllers();
-builder.WebHost.UseSentry();
-
-
-var featureManager = services.BuildServiceProvider().GetService<IFeatureManager>();
-if (await featureManager.IsEnabledAsync(FeatureFlags.OPEN_TELEMETRY))
-{
-    builder.Services.AddConsoleOpenTelemetry(config.GetSection("OpenTelemetryOptions"));
-}
-
-if (await featureManager.IsEnabledAsync(FeatureFlags.RABBITMQ))
-{
-
-}
-
-    //Options 
-    builder.Services.Configure<WebhookOptions>(builder.Configuration.GetSection("Webhook"));
-
-//Add kafka
-builder.Services.Configure<KafkaOption>(builder.Configuration.GetSection("Kafka"));
-builder.Services.Configure<KafkaProduceOption>(builder.Configuration.GetSection("KafkaProducer"));
-services
-    .AddSilverback()
-    // Use Apache Kafka as message broker
-    .WithConnectionToMessageBroker(
-        options => options
-            .AddKafka())
-
-    // Delegate the inbound/outbound endpoints configuration to a separate
-    // class.
-    .AddEndpointsConfigurator<EndpointsConfigurator>();
-
-builder.Services.AddSingleton<WebhookRegistrator>();
-
-//Add auto register webhooks
-if (config.GetSection("Webhook:AutoRegister")?.Value == "True")
-{
-    builder.Services.AddQuartzJobs();
-    services.AddSingletonJob<AutoRegisterWebhookJob>("*/59 * * * * ?"); //Every minute try register webhook again
-}
-
-//Add mluvii client
-var section = builder.Configuration.GetSection("Mluvii");
-if (section.Exists())
-{
-    builder.Services.Configure<MluviiCredentialOptions>(section);
-    builder.Services.AddSingleton<ICacheService, InMemoryCache>();
-    builder.Services.AddSingleton<ITokenEndpoint, TokenEndpoint>();
-    builder.Services.AddSingleton<MluviiClient>();
-    builder.Services.AddSingleton<IMluviiClient>(x => x.GetService<MluviiClient>());
-    builder.Services.AddSingleton<IMluviiUserClient>(x => x.GetService<MluviiClient>());
-}
-
-builder.Services.AddSingleton<MluviiWebhookHealthCheck>();
-builder.Services.AddHealthChecks()
-    .AddCheck<MluviiWebhookHealthCheck>("Webhook");
-
+ConfigureServices(builder);
 var app = builder.Build();
+ConfigurePipeline(app);
+app.Run();
 
-if (config.GetSection("Sentry").Exists())
+void ConfigureServices(WebApplicationBuilder builder)
 {
-    app.UseSentryTracing();
-    SentrySdk.CaptureMessage("Webhook service started!");
+    var services = builder.Services;
+    var config = builder.Configuration;
+    builder.WebHost.UseSentry();
+    services.AddFeatureManagement();
+    services.AddLogging(config);
+    services.AddControllers();
+    services.ConfigureTelemetry(config);
+    services.ConfigureMluviiClient(config);
+    services.ConfigureRabbitMQ(config);
+    services.ConfigureKafka(config);
+    services.ConfigureWebhooks(config, builder);
+    services.AddHealthChecks().AddCheck<MluviiWebhookHealthCheck>("Webhook");
+    builder.Services.AddSingleton<MluviiWebhookHealthCheck>();
 }
 
-// Configure the HTTP request pipeline.
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
+void ConfigurePipeline(WebApplication app)
+{
+    var config = app.Configuration;
 
-app.MapControllers();
-app.MapHealthChecks("/health");
+    if (config.GetSection("Sentry").Exists())
+    {
+        app.UseSentryTracing();
+        SentrySdk.CaptureMessage("Webhook service started!");
+    }
 
+    app.UseRouting();
+    app.UseAuthentication();
+    app.UseAuthorization();
 
-app.Run();
+    app.MapControllers();
+    app.MapHealthChecks("/health");
+}
 
 public partial class Program
 {
---
+++ APIGateway.MluviiWebhook/ServiceExtensions.cs
@@ -0,0 +1,102 @@
+﻿using APIGateway.Core;
+using APIGateway.Core.Cache;
+using APIGateway.Core.Kafka;
+using APIGateway.Core.MluviiClient;
+using APIGateway.MluviiWebhook;
+using APIGateway.MluviiWebhook.Jobs;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
+using Sentry;
+using Silverback.Samples.Kafka.Batch.Producer;
+
+namespace APIGateway.MluviiWebhook
+{
+    public static class ServiceExtensions
+    {
+        public static void AddLogging(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddLogging(builder =>
+                builder
+                    .AddDebug()
+                    .AddConsole()
+                    .AddConfiguration(config.GetSection("Logging"))
+                    .SetMinimumLevel(LogLevel.Information));
+        }
+
+        public static async Task ConfigureTelemetry(this IServiceCollection services, IConfiguration config)
+        {
+            var featureManager = services.BuildServiceProvider().GetService<IFeatureManager>();
+            if (await featureManager.IsEnabledAsync(FeatureFlags.OPEN_TELEMETRY))
+            {
+                services.AddConsoleOpenTelemetry(config.GetSection("OpenTelemetryOptions"));
+            }
+        }
+
+        public static async Task ConfigureRabbitMQ(this IServiceCollection services, IConfiguration config)
+        {
+            var featureManager = services.BuildServiceProvider().GetService<IFeatureManager>();
+            if (await featureManager.IsEnabledAsync(FeatureFlags.RABBITMQ))
+            {
+                services.AddHttpClient();
+                services.AddMassTransit(x =>
+                {
+                    x.UsingRabbitMq((context, cfg) =>
+                    {
+                        var rabbitMQConfig = context.GetRequiredService<IConfiguration>().GetSection("RabbitMQ");
+                        cfg.Host(rabbitMQConfig["Host"], rabbitMQConfig["VirtualHost"], h =>
+                        {
+                            h.Username(rabbitMQConfig["Username"]);
+                            h.Password(rabbitMQConfig["Password"]);
+                        });
+
+                        cfg.ConfigureEndpoints(context);
+                    });
+                });
+            }
+        }
+
+        public static void ConfigureKafka(this IServiceCollection services, IConfiguration config)
+        {
+            //Add kafka
+            services.Configure<KafkaOption>(config.GetSection("Kafka"));
+            services.Configure<KafkaProduceOption>(config.GetSection("KafkaProducer"));
+            services
+                .AddSilverback()
+                // Use Apache Kafka as message broker
+                .WithConnectionToMessageBroker(
+                    options => options
+                        .AddKafka())
+
+                // Delegate the inbound/outbound endpoints configuration to a separate
+                // class.
+                .AddEndpointsConfigurator<EndpointsConfigurator>();
+        }
+
+        public static void ConfigureWebhooks(this IServiceCollection services, IConfiguration config, WebApplicationBuilder builder)
+        {
+            services.Configure<WebhookOptions>(builder.Configuration.GetSection("Webhook"));
+            services.AddSingleton<WebhookRegistrator>();
+            //Add auto register webhooks
+            if (config.GetSection("Webhook:AutoRegister")?.Value == "True")
+            {
+                services.AddQuartzJobs();
+                services.AddSingletonJob<AutoRegisterWebhookJob>("*/59 * * * * ?"); //Every minute try register webhook again
+            }
+        }
+
+        public static void ConfigureMluviiClient(this IServiceCollection services, IConfiguration config)
+        {
+            var section = config.GetSection("Mluvii");
+            if (section.Exists())
+            {
+                services.Configure<MluviiCredentialOptions>(section);
+                services.AddSingleton<ICacheService, InMemoryCache>();
+                services.AddSingleton<ITokenEndpoint, TokenEndpoint>();
+                services.AddSingleton<MluviiClient>();
+                services.AddSingleton<IMluviiClient>(x => x.GetService<MluviiClient>());
+                services.AddSingleton<IMluviiUserClient>(x => x.GetService<MluviiClient>());
+            }
+        }
+    }
+}
