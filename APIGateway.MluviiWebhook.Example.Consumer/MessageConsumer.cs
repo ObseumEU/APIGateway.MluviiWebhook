83145641b4d5ae9d3cc41b5e9df5560891b12020
David Rosko <rosko.dav@gmail.com>
2023-12-26 23:58:01 +0100

    Add consumer example

---
+++ APIGateway.MluviiWebhook.Example.Consumer/MessageConsumer.cs
@@ -0,0 +1,13 @@
+﻿using MassTransit;
+using Newtonsoft.Json;
+
+namespace APIGateway.MluviiWebhook.Example.Consumer
+{
+    public class MessageConsumer : IConsumer<Contracts.WebhookEvent>
+    {
+        public async Task Consume(ConsumeContext<Contracts.WebhookEvent> context)
+        {
+            Console.WriteLine($"Receive: {JsonConvert.SerializeObject(context)}");
+        }
+    }
+}
---
+++ APIGateway.MluviiWebhook.Example.Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using APIGateway.MluviiWebhook.Example.Consumer;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHostEnvironment env = Host.CreateDefaultBuilder(args).Build().Services.GetRequiredService<IHostEnvironment>();
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, true)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, true)
+    .AddEnvironmentVariables()
+    .Build();
+
+
+Host.CreateDefaultBuilder(args)
+     .ConfigureServices((hostContext, services) =>
+     {
+         services.AddMassTransit(x =>
+         {
+             x.AddConsumer<MessageConsumer>();
+             x.UsingRabbitMq((context, cfg) =>
+             {
+                 var rabbitMQConfig = context.GetRequiredService<IConfiguration>().GetSection("RabbitMQ");
+                 cfg.Host(rabbitMQConfig["Host"], rabbitMQConfig["VirtualHost"], h =>
+                 {
+                     h.Username(rabbitMQConfig["Username"]);
+                     h.Password(rabbitMQConfig["Password"]);
+                 });
+
+                 cfg.ConfigureEndpoints(context);
+             });
+         });
+     })
+     .Build()
+     .Run();
