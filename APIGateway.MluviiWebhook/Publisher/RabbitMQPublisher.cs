765a3bf29f1fa45a4de1c8a5db5abbb478fa6074
David Rosko <rosko.dav@gmail.com>
2024-01-02 23:41:59 +0100

    Add configurable topic for rabbitMQ

--- APIGateway.MluviiWebhook/Publisher/RabbitMQPublisher.cs
+++ APIGateway.MluviiWebhook/Publisher/RabbitMQPublisher.cs
@@ -1,5 +1,6 @@
 ﻿using APIGateway.MluviiWebhook.Contracts;
 using MassTransit;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Linq;
 
 namespace APIGateway.MluviiWebhook.Publisher
@@ -7,10 +8,12 @@ namespace APIGateway.MluviiWebhook.Publisher
     public class RabbitMQPublisher : IMessagePublisher
     {
         private readonly IPublishEndpoint _publishEndpoint;
+        private readonly IOptions<RabbitMQOptions> _options;
 
-        public RabbitMQPublisher(IPublishEndpoint publishEndpoint)
+        public RabbitMQPublisher(IPublishEndpoint publishEndpoint, IOptions<RabbitMQOptions> options)
         {
             _publishEndpoint = publishEndpoint;
+            _options = options;
         }
 
         public async Task PublishAsync(JObject jobj)
@@ -21,7 +24,13 @@ namespace APIGateway.MluviiWebhook.Publisher
                 JsonData = jobj["data"].ToString()
             };
 
-            await _publishEndpoint.Publish<WebhookEvent>(payload);
+            await _publishEndpoint.Publish<WebhookEvent>(payload, ctx =>
+            {
+                if (!string.IsNullOrEmpty(_options.Value?.Topic))
+                {
+                    ctx.SetRoutingKey(_options.Value.Topic);
+                }
+            });
         }
     }
 }
